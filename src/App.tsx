890abec5c292cfc876094b46e31d0937095f069a
rpamaong <rpamaong@startechup.com>
2021-03-20 01:03:19 +0800

    setup prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="md:container md:mx-auto">
-      <p className="font-serif ...">carrier pigeon messenger</p>
-    </div>
-  );
+   return (
+      <div className="md:container md:mx-auto">
+         <div className="flex space-x-4 ">
+            <div className="flex-none w-2/6">
+               <h1>LEFT</h1>
+            </div>
+            <div className="flex-1">CENTER</div>
+            <div className="flex-none w-1/6">RIGHT</div>
+         </div>
+      </div>
+   );
 }
 
 export default App;
