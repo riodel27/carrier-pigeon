54c261b944bbab142f58cf78e8f2cb3bf8d7934f
rpamaong <rpamaong@startechup.com>
2021-03-22 03:13:56 +0800

    updated left side component and readme

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 import logo from './assets/pigeon-logo.png';
 import userImage from './assets/resources/user-one.jpeg';
 import userTwo from './assets/resources/user-two.jpeg';
-import userThree from './assets/resources/user-three.jpeg';
-import userFour from './assets/resources/user-four.jpeg';
+import userSix from './assets/resources/user-six.jpeg';
+import userFive from './assets/resources/user-five.jpeg';
 import settingsIcon from './assets/settings.png';
 
 function App() {
@@ -28,39 +28,53 @@ function App() {
                   <img className="items-start w-4 h-4 mt-1" src={settingsIcon} alt="" />
                </div>
                <div className="mt-5">
-                  <p>Active Chats</p>
-                  <input className="mt-5 mb-5" type="text" placeholder="Search People" />
+                  <p className="text-sm font-semibold text-gray-600">Active Chats</p>
+                  <input
+                     className="w-full pt-1 pb-1 pl-4 mt-5 mb-5 text-sm font-medium border border-gray-400 rounded-md bg-gray-50"
+                     type="text"
+                     placeholder="Search people"
+                  />
 
-                  <div className="flex justify-between mb-4">
-                     <img className="rounded-full w-14 h-14" src={userTwo} alt="" />
-                     <div>
-                        <p className="text-sm">Sasha Cohen</p>
-                        <p className="text-xs text-gray-400">Typing</p>
+                  <div className="flex items-center mb-4 ">
+                     <img className="w-12 h-12 mr-4 rounded-full" src={userTwo} alt="" />
+                     <div className="flex justify-between w-full">
+                        <div>
+                           <p className="text-sm font-semibold text-gray-600">Sasha Cohen</p>
+                           <p className="text-xs italic font-medium text-gray-500">Typing...</p>
+                        </div>
+                        <p className="text-xs font-medium text-gray-600">08:05</p>
                      </div>
-                     <p>08:05</p>
                   </div>
 
-                  <div className="flex justify-between mb-4">
-                     <img className="rounded-full w-14 h-14" src={userThree} alt="" />
-                     <div>
-                        <p className="text-sm">Sasha Cohen</p>
-                        <p className="text-xs text-gray-400">Typing</p>
+                  <div className="flex items-center mb-4 ">
+                     <img className="w-12 h-12 mr-4 rounded-full" src={userFive} alt="" />
+                     <div className="flex justify-between w-full">
+                        <div>
+                           <p className="text-sm font-semibold text-gray-600">Carmen Sue</p>
+                           <p className="text-xs font-medium text-gray-400">Hey!!</p>
+                        </div>
+                        <p className="text-xs font-medium text-gray-600">09:09</p>
                      </div>
-                     <p>08:05</p>
                   </div>
 
-                  <div className="flex justify-between mb-4">
-                     <img className="rounded-full w-14 h-14" src={userFour} alt="" />
-                     <div>
-                        <p className="text-sm">Sasha Cohen</p>
-                        <p className="text-xs text-gray-400">Typing</p>
+                  <div className="flex items-center mb-4 ">
+                     <img className="w-12 h-12 mr-4 rounded-full" src={userSix} alt="" />
+                     <div className="flex justify-between w-full">
+                        <div>
+                           <p className="text-sm font-semibold text-gray-600">Belle Rose</p>
+                           <p className="text-xs font-medium text-gray-400">Hello!</p>
+                        </div>
+                        <p className="text-xs font-medium text-gray-600">10:00</p>
                      </div>
-                     <p>08:05</p>
                   </div>
                </div>
             </div>
-            <div className="">message convo component</div>
-            <div className="">chat details component</div>
+            <div className="self-center flex-1">
+               <p className="text-center">Message Convo</p>
+            </div>
+            <div className="self-center flex-1">
+               <p className="text-center">chat Details</p>
+            </div>
          </div>
       </div>
    );
